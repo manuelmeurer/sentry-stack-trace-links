8fc6a5e3f9899970f17ce01ced31c8c9f056a894
Manuel Meurer <manuel@meurer.io>
2023-03-21 12:10:57 +0100

    refactor, handle existing links when the extension is loaded as well

--- content.js
+++ content.js
@@ -3,32 +3,28 @@ const localPaths = {
   "123": "/Users/me/myapp/"
 }
 
-addLinks = () => {
-  const callback = mutations => {
-    mutations.forEach(mutation => {
-      mutation.addedNodes.forEach(node => {
-        node.querySelectorAll("li.frame:not(.system-frame)").forEach(element => {
-          const filenameWrapper = element.querySelector(".filename")
-          const filename        = filenameWrapper.innerText
-          const line            = element.querySelector(".lineno").innerText
-
-          filenameWrapper.addEventListener("click", event => {
-            event.stopPropagation()
-            window.open(`vscode://file${localPath}${filename}:${line}`)
-          })
-        })
-      })
-    })
-  }
-
-  new MutationObserver(callback).observe(document.body, {
-    childList: true,
-    subtree:   true
-  })
-}
-
 const projectId = window.location.search.match(/project=(\d+)/)?.[1]
 const localPath = localPaths[projectId]
 
-if (localPath)
+const openVsCode = event => {
+  event.stopPropagation()
+
+  const filename = event.currentTarget.innerText
+  const line     = event.currentTarget.parentNode.querySelector(".lineno").innerText
+
+  window.open(`vscode://file${localPath}${filename}:${line}`)
+}
+
+const addLinks = () => {
+  document.querySelectorAll("li.frame:not(.system-frame) .filename")
+          .forEach(element => element.addEventListener("click", openVsCode))
+}
+
+if (localPath) {
   addLinks()
+  new MutationObserver(addLinks)
+    .observe(document.body, {
+      childList: true,
+      subtree:   true
+    })
+}
